a319400be10942e515a42808df406072faf1cd6d
Soner ATEŞ <captain_REX_camino@hotmail.com>
2021-03-11 18:20:33 +0300

    Add project files.

---
+++ MyDictonary/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class MyList<D>
+    {
+        D[] _array;
+        D[] _tempArray;
+
+        public MyList()
+        {
+            _array = new D[0];
+        }
+        public  void Add(D item)
+        {
+            _tempArray = _array;
+            _array = new D[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+    }
+}
---
+++ MyDictonary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Antalya");
+            sehirler.Add("Bursa");
+            sehirler.Add("İstanbul");
+            Console.WriteLine(sehirler.Count);
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Adana");
+            sehirler2.Add("Malatya");
+            sehirler2.Add("Adıyaman");
+            sehirler2.Add("Iğdır");
+            Console.WriteLine(sehirler2.Count);
+        }
+    }
+}
